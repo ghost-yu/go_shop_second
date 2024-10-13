0fac1e46f1ae84161d257baa2743e550c13881f1
ghost-yu <hgfhgfhgfhgfhgfhgf@yeah.net>
2024-10-14 01:21:55 +0800

    order stock inmem repo

---
+++ internal/order/domain/order/order.go
@@ -0,0 +1,11 @@
+package order
+
+import "github.com/ghost-yu/go_shop_second/common/genproto/orderpb"
+
+type Order struct {
+	ID          string
+	CustomerID  string
+	Status      string
+	PaymentLink string
+	Items       []*orderpb.Item
+}
