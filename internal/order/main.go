22e6797258d2b2c7f748fe559c86104282c08e56
ghost-yu <hgfhgfhgfhgfhgfhgf@yeah.net>
2024-10-13 18:46:42 +0800

    config, proto, openapi

---
+++ internal/order/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ghost-yu/go_shop_second/common/config"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	if err := config.NewViperConfig(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	log.Printf("%v", viper.Get("order"))
+}
