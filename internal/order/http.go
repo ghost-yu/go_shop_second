d9ad747927ce89ee5e0ba701e363cf09122887fb
ghost-yu <hgfhgfhgfhgfhgfhgf@yeah.net>
2024-10-14 21:07:45 +0800

    first Query

--- internal/order/http.go
+++ internal/order/http.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/ghost-yu/go_shop_second/order/app"
+	"github.com/ghost-yu/go_shop_second/order/app/query"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +18,13 @@ func (H HTTPServer) PostCustomerCustomerIDOrders(c *gin.Context, customerID stri
 }
 
 func (H HTTPServer) GetCustomerCustomerIDOrdersOrderID(c *gin.Context, customerID string, orderID string) {
-	//TODO implement me
-	panic("implement me")
+	o, err := H.app.Queries.GetCustomerOrder.Handle(c, query.GetCustomerOrder{
+		OrderID:    "fake-ID",
+		CustomerID: "fake-customer-id",
+	})
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "success", "data": o})
 }
--- internal/order/service/application.go
+++ internal/order/service/application.go
@@ -3,9 +3,21 @@ package service
 import (
 	"context"
 
+	"github.com/ghost-yu/go_shop_second/common/metrics"
+	"github.com/ghost-yu/go_shop_second/order/adapters"
 	"github.com/ghost-yu/go_shop_second/order/app"
+	"github.com/ghost-yu/go_shop_second/order/app/query"
+	"github.com/sirupsen/logrus"
 )
 
 func NewApplication(ctx context.Context) app.Application {
-	return app.Application{}
+	orderRepo := adapters.NewMemoryOrderRepository()
+	logger := logrus.NewEntry(logrus.StandardLogger())
+	metricClient := metrics.TodoMetrics{}
+	return app.Application{
+		Commands: app.Commands{},
+		Queries: app.Queries{
+			GetCustomerOrder: query.NewGetCustomerOrderHandler(orderRepo, logger, metricClient),
+		},
+	}
 }
